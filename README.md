859c3bb51c1bd85f4b66b3f97d4d05b721a8be98
rufait77 <rufaitislam@gmail.com>
2024-04-07 07:26:03 +0600

    update 4

--- README.md
+++
@@ -1,3 +0,0 @@
-# Create Responsive Sidebar Menu | Sidebar Bootstrap 4
-
-This is a sidebar menu by Rufait
--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -3,20 +3,3 @@ const hamBurger = document.querySelector(".toggle-btn");
 hamBurger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
 });
-
-
-
-$(document).ready(function(){
-  // Get current year
-  var currentYear = new Date().getFullYear();
-  // Set year range, you can adjust this range as needed
-  var startYear = currentYear; 
-  var endYear = currentYear + 5; 
-  // Generate years dropdown
-  var yearsOptions = '';
-  for(var year = startYear; year <= endYear; year++) {
-    yearsOptions += '<option value="' + year + '">' + year + '</option>';
-  }
-  // Append years dropdown to select element
-  $('#year').html(yearsOptions);
-});
