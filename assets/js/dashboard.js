d1db4983b3b0bae08345f34664e8c8b95afcb1c8
rufait77 <rufaitislam@gmail.com>
2024-04-07 20:37:00 +0600

    Tidal Table and Map form added

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -3,3 +3,18 @@ const hamBurger = document.querySelector(".toggle-btn");
 hamBurger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
 });
+
+$(document).ready(function () {
+  $('#dateFrom').datepicker({
+      format: 'yyyy-mm-dd',
+      autoclose: true
+  });
+});
+
+// Initialize Datepicker for Date Up to input field
+$(document).ready(function () {
+  $('#dateTo').datepicker({
+      format: 'yyyy-mm-dd',
+      autoclose: true
+  });
+});
